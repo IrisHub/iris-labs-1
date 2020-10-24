9a6a9bef3114e596a8f886533aac68f227b64cb4
Shalin Shah <shalinvs@gmail.com>
2020-10-23 20:54:51 -0700

    Initial Commit

---
+++ IrisLabs1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IrisLabs1
+//
+//  Created by Shalin on 10/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
