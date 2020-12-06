954c04fcc8043c0b4aea92f3ca0a89f358bed3be
Kanyes Thaker <krthaker1224@berkeley.edu>
2020-12-06 15:47:55 -0800

    bugfix

--- fax/api.py
+++ fax/api.py
@@ -1,6 +1,12 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+from datetime import datetime
+import pytz
+from datetime import timedelta
+# from ip2geotools.databases.noncommercial import DbIpCity
 from util import *
+import json
+import random
 
 def fax(event, context):
 	t = table_init("fax")
@@ -13,13 +19,45 @@ def fax(event, context):
 			":v": 1,
 		}
 	)
-	return get_count(None, None)
+
+	# if 'ip' in event:
+	# 	city = DbIpCity.get(event['ip'], api_key='free').city
+	# 	t.update_item(
+	# 		Key = {
+	# 			"id":"main",
+	# 		},
+	# 		UpdateExpression = "SET fax_city = :i, timestamp = :t",
+	# 		ExpressionAttributeValues = {
+	# 			":i":city,
+	# 			":t":int(time.time())
+	# 		}
+	# 	)
+
+	# 	return {
+	# 		"count":get_count(None, None),
+	# 		"last_updated": f"LATEST FAX: {pprint_time(int(time.time()))} AGO IN {city}"
+	# 	}
+	return {
+		"count": get_count(None, None),
+		"people": get_count(None, None)//13,
+		"date": datetime.strftime(datetime.now(pytz.timezone('US/Pacific')), '%B %d, %Y').upper(),
+		"time":datetime.strftime(datetime.now(pytz.timezone('US/Pacific')), '%I:%M %p')
+	}
 
 def get_count(event, context):
 	t = table_init("fax")
-	count = int(t.get_item(
+	item = t.get_item(
 		Key={
 			"id":"main"
 		}
-	)['Item']['fax_count'])
-	return count * 10
+	)['Item']
+	count = int(item['fax_count'])
+	# last_time = int(item['timestamp'])
+	# fax_city = item['fax_city']
+	# diff = int(time.time()) - last_time
+	# if diff > 1500: diff = random.randint(50, 1500)
+	# return {
+	# 		"count":count,
+	# 		"last_updated": f"LATEST FAX: {pprint_time()} AGO IN {city}"
+	# 	}
+	return count * 13
